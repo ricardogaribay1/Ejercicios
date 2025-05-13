d8c2acb9bc96aa6cd2d3feb634e5fcfbb8c23eed
Ricardo Garibay <ricardogaribay@encit.unam.mx>
2025-05-13 04:08:00 -0600

    BD

---
+++ mi-proyecto-node-mysql/src/models/user.js
@@ -0,0 +1,29 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../bd/database');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'user',
+  }
+);
+
+module.exports = { User };
